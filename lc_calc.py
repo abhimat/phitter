Fixing bug in error handling during light curve calculation

lc_calc.py
@@ -104,7 +104,7 @@ def binary_star_lc(star1_params, star2_params, binary_params, observation_times,
     # Read in the parameters of the binary system
     (binary_period, binary_ecc, binary_inc, t0) = binary_params
     
-    err_out = (np.array([-1.]), np.array([-1.]))
+    err_out = np.array([-1.]), np.array([-1.])
     
     
     # Set up binary model
@@ -245,7 +245,7 @@ def binary_star_lc(star1_params, star2_params, binary_params, observation_times,
         b.run_compute(compute='detailed', model='run')
     except:
         # print('Failure during compute')
-        return err_out
+        return (err_out, err_out)
     
     
     # Save out mesh plot

mcmc_fit.py
@@ -126,8 +126,6 @@ class mcmc_fitter_rad_interp(object):
          binary_inc_t, binary_period_t,
          binary_ecc_t, t0_t) = theta
         
-        err_out = np.array([-1.])
-        
         # Add units to input parameters if necessary
         binary_inc = binary_inc_t * u.deg
         binary_period = binary_period_t * u.d
@@ -162,7 +160,14 @@ class mcmc_fitter_rad_interp(object):
         
         # Run single star model for reference flux calculations
         (star1_sing_mag_Kp, star1_sing_mag_H) = lc_calc.single_star_lc(star1_params_lcfit)
+        
+        if (star1_sing_mag_Kp[0] == -1.) or (star1_sing_mag_H[0] == -1.):
+            return -np.inf
+        
         (star2_sing_mag_Kp, star2_sing_mag_H) = lc_calc.single_star_lc(star2_params_lcfit)
+        
+        if (star2_sing_mag_Kp[0] == -1.) or (star2_sing_mag_H[0] == -1.):
+            return -np.inf
         
         ## Apply distance modulus and isoc. extinction to single star magnitudes
         (star1_sing_mag_Kp, star1_sing_mag_H) = lc_calc.dist_ext_mag_calc(
@@ -170,16 +175,12 @@ class mcmc_fitter_rad_interp(object):
                                                     star1_sing_mag_H),
                                                     self.dist,
                                                     self.Kp_ext, self.H_ext)
-        if (star1_sing_mag_Kp == err_out) or (star1_sing_mag_H == err_out):
-            return -np.inf
         
         (star2_sing_mag_Kp, star2_sing_mag_H) = lc_calc.dist_ext_mag_calc(
                                                     (star2_sing_mag_Kp,
                                                     star2_sing_mag_H),
                                                     self.dist,
                                                     self.Kp_ext, self.H_ext)
-        if (star2_sing_mag_Kp == err_out) or (star2_sing_mag_H == err_out):
-            return -np.inf
         
         
         # Run binary star model to get binary mags
@@ -188,7 +189,7 @@ class mcmc_fitter_rad_interp(object):
                                               star2_params_lcfit,
                                               binary_params,
                                               self.observation_times)
-        if (binary_mags_Kp == err_out) or (binary_mags_H == err_out):
+        if (binary_mags_Kp[0] == -1.) or (binary_mags_H[0] == -1.):
             return -np.inf
         
         ## Apply distance modulus and isoc. extinction to binary magnitudes