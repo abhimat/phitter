605f1d4d82bd402a6bb3ca5c4f50dd6fb0b7b2f0
Abhimat Gautam <abhimat.gautam@gmail.com>
2019-11-19 22:56:32 -0800

    Update for stellar parameters calculation

--- mcmc_helper_scripts/stellar_params/stellar_params_calc.py
+++ mcmc_helper_scripts/stellar_params/stellar_params_calc.py
@@ -10,13 +10,15 @@ from astropy import units as u
 from astropy import constants as const
 from astropy.table import Table, vstack
 
+from emcee import backends
+
 from phoebe_phitter import isoc_interp
 
 import os
 import cPickle as pickle
 from tqdm import tqdm
 
-isoc_age = 10.0e9
+isoc_age = 10e9
 isoc_ext = 2.63
 isoc_dist = 7.971e3
 isoc_phase = 'RGB'
@@ -26,43 +28,65 @@ isoc_atm_func = 'phoenix'
 # Read in data
 trial_num = 1
 
-chains_table = Table.read('../chains/chains_try{0}.dat'.format(trial_num), format='ascii')
+chains_file = '../chains/chains_try{0}.h5'.format(trial_num)
+reader = backends.HDFBackend(chains_file, read_only=True)
 
+samples = reader.get_chain()
+(num_steps, num_chains, num_params) = samples.shape
+samples = reader.get_chain(flat=True)
+
+print("Number of Steps: {0}".format(num_steps))
+print("Number of Chains: {0}".format(num_chains))
+print("Number of Parameters: {0}".format(num_params))
+
+log_prob_samples = reader.get_log_prob(flat=True)
+log_prior_samples = reader.get_blobs(flat=True)
+
+
+# Check if any rows can be ignored that have been calculated already
 rows_ignore = 0
 
-chain_nums = np.unique(chains_table['col1'])
-num_walkers = len(chain_nums)
-
 ## If the data file already exists, start reading for new data
-if os.path.exists('stellar_params.hdf5'):
+if os.path.exists('stellar_params.h5'):
     import h5py
-    f = h5py.File('stellar_params.hdf5', 'r')
+    f = h5py.File('stellar_params.h5', 'r')
     rows_ignore, = f['data'].shape
     
-    prev_params_table = Table.read('stellar_params.hdf5', path='data')
+    prev_params_table = Table.read('stellar_params.h5', path='data')
 
 # Read in light curve data
 target_star = 'S2-36'
 with open('../lc_data.pkl', 'rb') as input_pickle:
     target_binary_period = pickle.load(input_pickle)
+    phase_shift_fit = pickle.load(input_pickle)
     kp_target_mags = pickle.load(input_pickle)
     kp_target_mag_errors = pickle.load(input_pickle)
+    kp_target_MJDs = pickle.load(input_pickle)
     kp_phased_days = pickle.load(input_pickle)
-    kp_phase_shift_fit = pickle.load(input_pickle)
+    h_target_mags = pickle.load(input_pickle)
+    h_target_mag_errors = pickle.load(input_pickle)
+    h_target_MJDs = pickle.load(input_pickle)
+    h_phased_days = pickle.load(input_pickle)
 
+
+num_observations = len(kp_target_mags) + len(h_target_mags)
+print("Number of Observations: {0}".format(num_observations))
 
 # Samples of fitted parameters
-K_ext_samps = (chains_table['col2'])[rows_ignore:]
-star1_rad_samps = (chains_table['col3'])[rows_ignore:]
-star2_rad_samps = (chains_table['col4'])[rows_ignore:]
-binary_inc_samps = (chains_table['col5'])[rows_ignore:]
-binary_per_samps = (chains_table['col6'])[rows_ignore:]
-binary_dist_samps = (chains_table['col7'])[rows_ignore:]
-t0_samps = (chains_table['col8'])[rows_ignore:]
+K_ext_samps = samples[:, 0][rows_ignore:]
+H_ext_mod_samps = samples[:, 1][rows_ignore:]
+star1_rad_samps = samples[:, 2][rows_ignore:]
+star2_rad_samps = samples[:, 3][rows_ignore:]
+binary_inc_samps = samples[:, 4][rows_ignore:]
+binary_per_samps = samples[:, 5][rows_ignore:]
+# binary_dist_samps = samples[:, 5][rows_ignore:]
+t0_samps = samples[:, 6][rows_ignore:]
+
+log_prob_samps = log_prob_samples[rows_ignore:]
 
 num_samples = len(K_ext_samps)
 
-parameter_names = ['K_ext', 'rad_1', 'rad_2', 'inc', 'period', 'dist', 't0']
+parameter_names = ['K_ext', 'H_ext_mod', 'rad_1', 'rad_2', 'inc', 'period', 't0']
 
 # Other stellar and binary parameters to derive
 ## Stellar parameters, to be derived from isochrone
@@ -70,6 +94,7 @@ star1_mass_init_samps = np.empty(num_samples)
 star1_mass_samps = np.empty(num_samples)
 star1_lum_samps = np.empty(num_samples)
 star1_teff_samps = np.empty(num_samples)
+star1_logg_samps = np.empty(num_samples)
 star1_mag_Kp_samps = np.empty(num_samples)
 star1_mag_H_samps = np.empty(num_samples)
 star1_pblum_Kp_samps = np.empty(num_samples)
@@ -79,6 +104,7 @@ star2_mass_init_samps = np.empty(num_samples)
 star2_mass_samps = np.empty(num_samples)
 star2_lum_samps = np.empty(num_samples)
 star2_teff_samps = np.empty(num_samples)
+star2_logg_samps = np.empty(num_samples)
 star2_mag_Kp_samps = np.empty(num_samples)
 star2_mag_H_samps = np.empty(num_samples)
 star2_pblum_Kp_samps = np.empty(num_samples)
@@ -90,7 +116,8 @@ binary_sma_samps_solRad = np.empty(num_samples)
 binary_q_samps = np.empty(num_samples)
 binary_q_init_samps = np.empty(num_samples)
 
-
+## Fit Characteristics
+fit_chi2red_samps = np.empty(num_samples)
 
 # Generate PopStar isochrone
 from popstar import synthetic, evolution, atmospheres, reddening
@@ -109,38 +136,40 @@ for cur_samp_num in tqdm(range(num_samples)):
     cur_star1_rad = star1_rad_samps[cur_samp_num]
     cur_star2_rad = star2_rad_samps[cur_samp_num]
     cur_binary_period = binary_per_samps[cur_samp_num] * u.d
-    
+
     ## Stellar parameters from isochrone
     (star1_params_all, star1_params_lcfit) = isochrone.rad_interp(cur_star1_rad)
     (star2_params_all, star2_params_lcfit) = isochrone.rad_interp(cur_star2_rad)
-    
+
     (cur_star1_mass_init, cur_star1_mass, cur_star1_rad, cur_star1_lum,
         cur_star1_teff, cur_star1_logg, cur_star1_mag_Kp, cur_star1_mag_H,
         cur_star1_pblum_Kp, cur_star1_pblum_H) = star1_params_all
     (cur_star2_mass_init, cur_star2_mass, cur_star2_rad, cur_star2_lum,
-        cur_star2_teff, cur_star1_logg, cur_star2_mag_Kp, cur_star2_mag_H,
+        cur_star2_teff, cur_star2_logg, cur_star2_mag_Kp, cur_star2_mag_H,
         cur_star2_pblum_Kp, cur_star2_pblum_H) = star2_params_all
-    
+
     ## Binary parameters
     cur_binary_sma = ((cur_binary_period**2. * const.G * (cur_star1_mass + cur_star2_mass)) / (4. * np.pi**2.))**(1./3.)
-    
+
     cur_binary_q = cur_star2_mass / cur_star1_mass
     cur_binary_q_init = cur_star2_mass_init / cur_star1_mass_init
-    
+
     ## Store out all the values
     star1_mass_init_samps[cur_samp_num] = cur_star1_mass_init.to(u.solMass).value
     star1_mass_samps[cur_samp_num] = cur_star1_mass.to(u.solMass).value
     star1_lum_samps[cur_samp_num] = cur_star1_lum.to(u.solLum).value
     star1_teff_samps[cur_samp_num] = cur_star1_teff.to(u.K).value
+    star1_logg_samps[cur_samp_num] = cur_star1_logg
     star1_mag_Kp_samps[cur_samp_num] = cur_star1_mag_Kp
     star1_mag_H_samps[cur_samp_num] = cur_star1_mag_H
     star1_pblum_Kp_samps[cur_samp_num] = cur_star1_pblum_Kp.to(u.solLum).value
     star1_pblum_H_samps[cur_samp_num] = cur_star1_pblum_H.to(u.solLum).value
-    
+
     star2_mass_init_samps[cur_samp_num] = cur_star2_mass_init.to(u.solMass).value
     star2_mass_samps[cur_samp_num] = cur_star2_mass.to(u.solMass).value
     star2_lum_samps[cur_samp_num] = cur_star2_lum.to(u.solLum).value
     star2_teff_samps[cur_samp_num] = cur_star2_teff.to(u.K).value
+    star2_logg_samps[cur_samp_num] = cur_star2_logg
     star2_mag_Kp_samps[cur_samp_num] = cur_star2_mag_Kp
     star2_mag_H_samps[cur_samp_num] = cur_star2_mag_H
     star2_pblum_Kp_samps[cur_samp_num] = cur_star2_pblum_Kp.to(u.solLum).value
@@ -151,42 +180,50 @@ for cur_samp_num in tqdm(range(num_samples)):
     binary_sma_samps_solRad[cur_samp_num] = cur_binary_sma.to(u.solRad).value
     binary_q_samps[cur_samp_num] = cur_binary_q
     binary_q_init_samps[cur_samp_num] = cur_binary_q_init
+    
+    ## Fit Characteristics
+    fit_chi2red_samps[cur_samp_num] = (log_prob_samps[cur_samp_num] * -2.) / (num_observations - num_params)
+    
 
 
 
 # Make parameter table
-params_table = Table([K_ext_samps, star1_rad_samps*u.solRad, star2_rad_samps*u.solRad,
+params_table = Table([K_ext_samps, H_ext_mod_samps,
+                      star1_rad_samps*u.solRad, star2_rad_samps*u.solRad,
                       binary_inc_samps*u.deg, binary_per_samps*u.d,
-                      binary_dist_samps*u.pc, t0_samps,
+                      t0_samps,
                       star1_mass_init_samps*u.solMass, star1_mass_samps*u.solMass,
-                      star1_lum_samps*u.solLum, star1_teff_samps*u.K,
+                      star1_lum_samps*u.solLum, star1_teff_samps*u.K, star1_logg_samps,
                       star1_mag_Kp_samps, star1_mag_H_samps,
                       star1_pblum_Kp_samps*u.solLum, star1_pblum_H_samps*u.solLum,
                       star2_mass_init_samps*u.solMass, star2_mass_samps*u.solMass,
-                      star2_lum_samps*u.solLum, star2_teff_samps*u.K,
+                      star2_lum_samps*u.solLum, star2_teff_samps*u.K, star2_logg_samps,
                       star2_mag_Kp_samps, star2_mag_H_samps,
                       star2_pblum_Kp_samps*u.solLum, star2_pblum_H_samps*u.solLum,
-                      binary_sma_samps*u.AU, binary_q_samps, binary_q_init_samps],
-                     names=('K_ext', 'star1_rad', 'star2_rad',
+                      binary_sma_samps*u.AU, binary_q_samps, binary_q_init_samps,
+                      log_prob_samps, fit_chi2red_samps],
+                     names=('K_ext', 'H_ext_mod',
+                            'star1_rad', 'star2_rad',
                             'binary_inc', 'binary_per',
-                            'binary_dist', 't0',
+                            't0',
                             'star1_mass_init', 'star1_mass',
-                            'star1_lum', 'star1_teff',
+                            'star1_lum', 'star1_teff', 'star1_logg',
                             'star1_mag_Kp', 'star1_mag_H',
                             'star1_pblum_Kp', 'star1_pblum_H',
                             'star2_mass_init', 'star2_mass',
-                            'star2_lum', 'star2_teff',
+                            'star2_lum', 'star2_teff', 'star2_logg',
                             'star2_mag_Kp', 'star2_mag_H',
                             'star2_pblum_Kp', 'star2_pblum_H',
-                            'binary_sma', 'binary_q', 'binary_q_init'))
+                            'binary_sma', 'binary_q', 'binary_q_init',
+                            'log_prob', 'fit_chi2red'))
 
 
-if os.path.exists('stellar_params.hdf5'):
+if os.path.exists('stellar_params.h5'):
     params_table = vstack([prev_params_table, params_table])
-    
-    params_table.write('stellar_params.hdf5', path='data',
+
+    params_table.write('stellar_params.h5', path='data',
                        serialize_meta=True, compression=True,
                        overwrite=True)
 else:
-    params_table.write('stellar_params.hdf5', path='data',
+    params_table.write('stellar_params.h5', path='data',
                        serialize_meta=True, compression=True)
--- mcmc_helper_scripts/stellar_params/stellar_params_plotter.py
+++ mcmc_helper_scripts/stellar_params/stellar_params_plotter.py
@@ -26,16 +26,17 @@ rows_ignore = num_walkers*burn_ignore_len
 
 
 # Read in table of parameters
-params_table = Table.read('./stellar_params.hdf5', path='data')
+params_table = Table.read('./stellar_params.h5', path='data')
 num_samples = len(params_table)
 
 ## Read in stellar parameters
 K_ext_samps = params_table['K_ext'][rows_ignore:]
+H_ext_mod_samps = params_table['H_ext_mod'][rows_ignore:]
 star1_rad_samps = params_table['star1_rad'][rows_ignore:]
 star2_rad_samps = params_table['star2_rad'][rows_ignore:]
 binary_inc_samps = params_table['binary_inc'][rows_ignore:]
 binary_per_samps = params_table['binary_per'][rows_ignore:]
-binary_dist_samps = params_table['binary_dist'][rows_ignore:]
+# binary_dist_samps = params_table['binary_dist'][rows_ignore:]
 t0_samps = params_table['t0'][rows_ignore:]
 
 # Other stellar and binary parameters to derive
@@ -44,6 +45,7 @@ star1_mass_init_samps = params_table['star1_mass_init'][rows_ignore:]
 star1_mass_samps = params_table['star1_mass_init'][rows_ignore:]
 star1_lum_samps = params_table['star1_lum'][rows_ignore:]
 star1_teff_samps = params_table['star1_teff'][rows_ignore:]
+star1_logg_samps = params_table['star1_logg'][rows_ignore:]
 star1_mag_Kp_samps = params_table['star1_mag_Kp'][rows_ignore:]
 star1_mag_H_samps = params_table['star1_mag_H'][rows_ignore:]
 star1_pblum_Kp_samps = params_table['star1_pblum_Kp'][rows_ignore:]
@@ -53,6 +55,7 @@ star2_mass_init_samps = params_table['star2_mass_init'][rows_ignore:]
 star2_mass_samps = params_table['star2_mass'][rows_ignore:]
 star2_lum_samps = params_table['star2_lum'][rows_ignore:]
 star2_teff_samps = params_table['star2_teff'][rows_ignore:]
+star2_logg_samps = params_table['star2_logg'][rows_ignore:]
 star2_mag_Kp_samps = params_table['star2_mag_Kp'][rows_ignore:]
 star2_mag_H_samps = params_table['star2_mag_H'][rows_ignore:]
 star2_pblum_Kp_samps = params_table['star2_pblum_Kp'][rows_ignore:]
@@ -63,6 +66,10 @@ binary_sma_samps = params_table['binary_sma'][rows_ignore:]
 binary_sma_samps_solRad = params_table['binary_sma'][rows_ignore:].to(u.solRad).value
 binary_q_samps = params_table['binary_q'][rows_ignore:]
 binary_q_init_samps = params_table['binary_q_init'][rows_ignore:]
+
+## Fit Characteristics
+log_prob_samps = params_table['log_prob'][rows_ignore:]
+fit_chi2red_samps = params_table['fit_chi2red'][rows_ignore:]
 
 ## Create inclination samples that are just between 0 and 90 degrees
 binary_inc_90max_samps = binary_inc_samps.copy()
@@ -173,6 +180,7 @@ param_samps_plotter(star1_mass_samps, 'star1_mass', r"$M_{1}$", r"$(M_{\odot})$"
 param_samps_plotter(star1_mass_init_samps, 'star1_mass_init', r"Init. $M_{1}$", r"$(M_{\odot})$", plot_kde=False)
 param_samps_plotter(star1_lum_samps, 'star1_lum', r"$L_{1}$", r"$(L_{\odot})$", plot_kde=False)
 param_samps_plotter(star1_teff_samps, 'star1_teff', r"Eff. $T_{1}$", r"(K)", plot_kde=False)
+param_samps_plotter(star1_logg_samps, 'star1_logg', r"$\\log g$", '', plot_kde=False)
 param_samps_plotter(star1_mag_Kp_samps, 'star1_mag_Kp', r"$m_{K'}$", '', plot_kde=False)
 param_samps_plotter(star1_mag_H_samps, 'star1_mag_H', r"$m_{H}$", '', plot_kde=False)
 
@@ -181,6 +189,7 @@ param_samps_plotter(star2_mass_samps, 'star2_mass', r"$M_{2}$", r"$(M_{\odot})$"
 param_samps_plotter(star2_mass_init_samps, 'star2_mass_init', r"Init. $M_{2}$", r"$(M_{\odot})$", plot_kde=False)
 param_samps_plotter(star2_lum_samps, 'star2_lum', r"$L_{2}$", r"$(L_{\odot})$", plot_kde=False)
 param_samps_plotter(star2_teff_samps, 'star2_teff', r"Eff. $T_{2}$", r"(K)", plot_kde=False)
+param_samps_plotter(star2_logg_samps, 'star2_logg', r"$\\log g$", '', plot_kde=False)
 param_samps_plotter(star2_mag_Kp_samps, 'star2_mag_Kp', r"$m_{K'}$", '', plot_kde=False)
 param_samps_plotter(star2_mag_H_samps, 'star2_mag_H', r"$m_{H}$", '', plot_kde=False)
 
@@ -190,13 +199,18 @@ param_samps_plotter(binary_inc_samps, 'binary_inc', r"$i$", r"$(\degree)$", plot
 param_samps_plotter(binary_inc_90max_samps, 'binary_inc_90max', r"$i$", r"$(\degree)$", plot_kde=False)
 param_samps_plotter(binary_q_samps, 'binary_q', r"$q = M_{2} / M_{1}$", '', plot_kde=False)
 param_samps_plotter(binary_q_init_samps, 'binary_q_init', r"Init. $M_{2} / M_{1}$", '', plot_kde=False)
-param_samps_plotter(binary_dist_samps/1000., 'binary_dist', r"$d$", r"(kpc)", plot_kde=False)
+# param_samps_plotter(binary_dist_samps/1000., 'binary_dist', r"$d$", r"(kpc)", plot_kde=False)
 
 
 param_samps_plotter(K_ext_samps, 'K_ext', r"$A_{K'}$", '', plot_kde=False)
+param_samps_plotter(H_ext_mod_samps, 'H_ext_mod', r"$A_{H}$ Mod", '', plot_kde=False)
 
 param_samps_plotter(binary_per_samps, 'period', "Period", '(d)', plot_kde=False)
 param_samps_plotter(t0_samps, 't0', r"$t_0$", '(MJD)', plot_kde=False)
+
+
+param_samps_plotter(log_prob_samps, 'log_prob', "Log Probability", '', plot_kde=False)
+param_samps_plotter(fit_chi2red_samps, 'fit_chi2red', r"$\chi^2_{\mathrm{red}}$", '', plot_kde=False)
 
 
 
