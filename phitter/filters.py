501c3213744d1e33a5bac66d889f3cf232c642d8
Abhimat Gautam <abhimat.gautam@gmail.com>
2022-07-22 13:04:49 -0700

    Adding support for phoebe passbands in filters

--- phitter/filters.py
+++ phitter/filters.py
@@ -10,6 +10,9 @@ lambda_Ks = 2.18e-6 * u.m
 
 class filter(object):
     def __init__(self):
+        self.filter_name = 'filt'
+        self.phoebe_ds_name = 'mod_lc_filt'
+        self.phoebe_pb_name = 'tel_inst:filt'
         
         # Filter properties
         self.lambda_filt = 0.0 * u.m
@@ -21,14 +24,17 @@ class filter(object):
         
         return
     
-    def calc_isoc_filt_ext(isoc_Ks_ext, ext_alpha):
+    def calc_isoc_filt_ext(self, isoc_Ks_ext, ext_alpha):
         isoc_filt_ext = isoc_Ks_ext *\
                         (lambda_Ks / self.lambda_filt)**ext_alpha
+        
+        return isoc_filt_ext
     
 class naco_ks_filt(filter):
     def __init__(self):
-        
         self.filter_name = 'naco,Ks'
+        self.phoebe_ds_name = 'mod_lc_Ks'
+        self.phoebe_pb_name = 'VLT_NACO:Ks'
         
         # Filter properties
         self.lambda_filt = 2.18e-6 * u.m
@@ -42,8 +48,9 @@ class naco_ks_filt(filter):
 
 class nirc2_lp_filt(filter):
     def __init__(self):
-        
         self.filter_name = 'nirc2,Lp'
+        self.phoebe_ds_name = 'mod_lc_Lp'
+        self.phoebe_pb_name = 'Keck_NIRC2:Lp'
         
         # Filter properties
         self.lambda_filt = 3.776e-6 * u.m
@@ -57,8 +64,9 @@ class nirc2_lp_filt(filter):
 
 class nirc2_kp_filt(filter):
     def __init__(self):
-        
         self.filter_name = 'nirc2,Kp'
+        self.phoebe_ds_name = 'mod_lc_Kp'
+        self.phoebe_pb_name = 'Keck_NIRC2:Kp'
         
         # Filter properties
         self.lambda_filt = 2.124e-6 * u.m
@@ -72,8 +80,9 @@ class nirc2_kp_filt(filter):
 
 class nirc2_h_filt(filter):
     def __init__(self):
-        
         self.filter_name = 'nirc2,H'
+        self.phoebe_ds_name = 'mod_lc_H'
+        self.phoebe_pb_name = 'Keck_NIRC2:H'
         
         # Filter properties
         self.lambda_filt = 1.633e-6 * u.m
