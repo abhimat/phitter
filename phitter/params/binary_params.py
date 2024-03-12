5495be311067ed28af920c06ed7bd38d6682e76d
Abhimat Gautam <abhimat.gautam@gmail.com>
2024-03-12 16:32:25 -0700

    Object to store parameters for binary system

---
+++ phitter/params/binary_params.py
@@ -0,0 +1,28 @@
+# Objects for star parameters
+
+from phoebe import u
+
+class binary_params(object):
+    """
+    binary_params is an object to store parameters describing a binary sytem.
+    
+    Attributes
+    ----------
+    period : Astropy Quantity, unit:d
+        Orbital period of binary in days.
+    ecc : float
+        Eccentricity of binary orbit.
+    inc : Astropy Quantity, unit:deg
+        Binary orbit inclination, relative to line of sight, in degrees.
+    t0 : float
+        Binary system's reference time, t0. Specified in MJDs. For eclipsing
+        systems, typically minimum of deepest eclipse.
+    """
+    
+    period = 0. * u.d
+    ecc = 0.
+    inc = 90. * u.deg
+    t0 = 48546.0
+    
+    def __init__(self):
+        return
