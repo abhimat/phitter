64bb0cac93e219530bb7d975f76852eb4056e369
Abhimat Gautam <abhimat.gautam@gmail.com>
2024-03-28 10:13:05 -0700

    Revised naming in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'phitter'
+project = 'Phitter'
 copyright = '2024, Abhimat K. Gautam'
 author = 'Abhimat K. Gautam'
 release = '0.2'
