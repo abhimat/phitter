a4f2b27c14b61634a1e872845eff03068ac5f8b4
Abhimat Gautam <abhimat.gautam@gmail.com>
2024-04-03 13:35:06 -0700

    New likelihood scheme

--- phitter/fit/likelihood.py
+++ phitter/fit/likelihood.py
@@ -80,4 +80,77 @@ class log_likelihood_chisq_weighted_obs_type(log_likelihood_chisq):
             return -1e300
         else:
             return log_likelihood
+
+class log_likelihood_chisq_weighted_filts_and_obs_type(log_likelihood_chisq):
+    """
+    log_likelihood_chisq_weighted_filts_and_obs_type is an object to obtain chi
+    squared log likelihood, while weighing different observation types, and
+    filters within each observation type equally.
+    This weighting scheme may be useful for cases where number of a given
+    observation type or filt is much larger than that of another.
     
+    The output log likelihood is calculated in the following way:
+    log_like_total = (log_like_t1)/(n_t1) + (log_like_t2)/(n_t2) + ...
+    (see e.g., Lam+ 2022)
+    
+    Instantiate this object with an observables object to indicate the observed
+    observables.
+    
+    Parameters
+    ----------
+    observations : observables
+        observables object, specified with the observations to be fit.
+    """
+    
+    def evaluate(self, model_observables):
+        log_likelihood = 0
+        
+        if self.observations.num_obs_phot > 0:
+            # Add likelihood for each filt in photometry
+            for cur_filt in self.observations.unique_filts_phot:
+                # Get search filter for the current photometry filt
+                cur_filt_search_filter = self.observations.phot_filt_filters[cur_filt]
+                
+                # Cut out relevant observations from observations and model
+                cur_filt_obs = self.observations.obs[cur_filt_search_filter]
+                cur_filt_obs_uncs = self.observations.obs_uncs[cur_filt_search_filter]
+                cur_filt_model_obs = model_observables.obs[cur_filt_search_filter]
+                
+                cur_filt_num_obs = len(cur_filt_obs)
+                
+                if cur_filt_num_obs > 0:
+                    # Compute likelihood and add to total
+                    log_likelihood += (-0.5 * np.sum(
+                        ((cur_filt_obs - cur_filt_model_obs) / \
+                         cur_filt_obs_uncs)**2.
+                    )) / cur_filt_num_obs
+                else:
+                    continue
+        
+        if self.observations.num_obs_rv > 0:
+            # Add likelihood for each filt in RV
+            for cur_filt in self.observations.unique_filts_rv:
+                # Get search filter for the current RV filt
+                cur_filt_search_filter = self.observations.rv_filt_filters[cur_filt]
+                
+                # Cut out relevant observations from observations and model
+                cur_filt_obs = self.observations.obs[cur_filt_search_filter]
+                cur_filt_obs_uncs = self.observations.obs_uncs[cur_filt_search_filter]
+                cur_filt_model_obs = model_observables.obs[cur_filt_search_filter]
+                
+                cur_filt_num_obs = len(cur_filt_obs)
+                
+                if cur_filt_num_obs > 0:
+                    # Compute likelihood and add to total
+                    log_likelihood += (-0.5 * np.sum(
+                        ((cur_filt_obs - cur_filt_model_obs) / \
+                         cur_filt_obs_uncs)**2.
+                    )) / cur_filt_num_obs
+                else:
+                    continue
+        
+        if np.isnan(log_likelihood):
+            return -1e300
+        else:
+            return log_likelihood
+            
